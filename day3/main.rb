bec30fd937d15549b83fc2a61695ce641062913a
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2024-12-04 17:48:10 +0100

    Solve day 3 part 1

---
+++ day3/main.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+input = File.read('input.txt')
+
+result = 0
+
+muls = input.scan(/mul\((\d{1,3}),(\d{1,3})\)/)
+muls.each do |m|
+  result += m[0].to_i *  m[1].to_i
+  puts "result: #{result}"
+end
+
+puts "Result: #{result}"
